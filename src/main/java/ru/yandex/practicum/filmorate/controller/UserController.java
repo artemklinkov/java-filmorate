be656c27b0dfa586faaedb95727ecc39eb5e43f6
Artem <artemklinkov@yandex.ru>
2022-08-01 16:42:45 +0300

    fix: обновить несуществующего пользователя, обновить несуществующий фильм

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -14,30 +14,30 @@ import java.util.Map;
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-    private final Map<String, User> users = new HashMap<>();
+    private final Map<Integer, User> users = new HashMap<>();
     private final Generator generator = new Generator();
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
         log.debug("Добавление пользователя {}", user);
-        if (users.get(user.getLogin()) == null) {
+        if (users.get(user.getId()) == null) {
             user.setId(generator.getNextID());
-            users.put(user.getLogin(), user);
+            users.put(user.getId(), user);
         } else {
             user.setId(users.get(user.getLogin()).getId());
-            users.put(user.getLogin(), user);
+            users.put(user.getId(), user);
         }
-        return users.get(user.getLogin());
+        return users.get(user.getId());
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
         log.debug("Обновление пользователя {}", user);
-        if (users.get(user.getLogin()) == null) {
+        if (users.get(user.getId()) == null) {
             throw new RuntimeException("Пользователь не найден");
         }
 
-        users.put(user.getLogin(), user);
+        users.put(user.getId(), user);
         return user;
     }
 
